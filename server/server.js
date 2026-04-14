1cc133638d8d4184472732f346b5ff50ccbc03d1
Opy-22 <raduan.ahmed.opy@g.bracu.ac.bd>
2026-04-14 10:26:49 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
-const dotenv = require('dotenv');
-dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config();
+const reportRoutes = require("./routes/reportroutes");
 
-const app = require('./src/app');
+const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/api/reports", reportRoutes);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
