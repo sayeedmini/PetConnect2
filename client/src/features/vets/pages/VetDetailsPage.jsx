dd0bc3928d5722cc64cc574b2977a533df08766b
Md Abu Sayeed Bhuiyan <ma.sayeed.b@gmail.com>
2026-04-15 17:28:59 +0600

    Add edit and delete functionality to vet directory

---
+++ client/src/features/vets/pages/VetDetailsPage.jsx
@@ -0,0 +1,10 @@
+function VetDetailsPage() {
+  return (
+    <div style={{ padding: '30px' }}>
+      <h1>Vet Details Page</h1>
+      <p>This page will show single clinic details later.</p>
+    </div>
+  );
+}
+
+export default VetDetailsPage;
