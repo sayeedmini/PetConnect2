5462773255fa9093fcc45c7bb046fe071b296191
Opy-22 <raduan.ahmed.opy@g.bracu.ac.bd>
2026-04-16 06:29:52 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const http = require('http');
 const path = require("path");
 require("dotenv").config();
 const reportRoutes = require("./routes/reportroutes");
 
 const app = express();
+const server = http.createServer(app);
+
+const { Server } = require('socket.io');
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PATCH"],
+  },
+});
+
+app.set("io", io);
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +31,14 @@ mongoose
 
 app.use("/api/reports", reportRoutes);
 
-app.listen(process.env.PORT || 5000, () => {
+io.on("connection", (socket) => {
+  console.log("A client connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+server.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
